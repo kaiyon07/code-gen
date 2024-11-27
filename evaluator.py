3f8497d7bf5bd2885d06eb122cba933412419c68
jaczhi <54284925+jaczhi@users.noreply.github.com>
2024-11-26 17:47:41 -0800

    quality of life improvements

--- evaluator.py
+++ evaluator.py
@@ -7,6 +7,7 @@ import json
 import csv
 import time
 import hashlib
+from tqdm import tqdm
 
 TEST_CASES_CACHE_PATH = "./js/test-cases.json"
 
@@ -183,7 +184,7 @@ def evaluate_batch(in_csv, out_csv, original_code_column: str="gt_code", output_
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for row in reader:
+        for row in tqdm(reader):
             try:
                 time.sleep(api_sleep_interval)
                 result = evaluate(row[original_code_column], row[output_code_column], run_code)
